d41dfaec7447bc2c2cdda273b02a67a32bdf7fa4
KumarB95 <kumarbalingal@gmail.com>
2021-09-23 08:33:56 +0530

    Modified : Created board with empty spaces ignored 0th Index

--- src/com/bridgelabz/program/TicTacToeGame.java
+++ src/com/bridgelabz/program/TicTacToeGame.java
@@ -1,7 +1,21 @@
 package com.bridgelabz.program;
+/**
+ * Program for TicTacToe Game playing with computer
+ * Author: Navalkumar
+ * Date: 23/09/2021
+ */
 
 public class TicTacToeGame {
+    static char[] board = new char[10];
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Program !");
+        createBoard();
+    }
+
+    public static void createBoard(){
+        for (int i=1; i<10;i++){
+                board[i]=' ';
+        }
     }
 }
